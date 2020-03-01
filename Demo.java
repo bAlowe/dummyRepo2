f68d60c6d2cc3cebb7e14e212faa79bffa0252d4
brittanylowe <brittany.lowe@students.cau.edu>
2020-02-29 21:35:22 -0500

    first push

---
+++ Demo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Demo{
+public staic void main(String[] args)
+{
+ProductionWorker pw1 = ProductionWorker("Jane", "456-M", "01/01/2015",2,25.00)
+
+System.out.println(pw1.getName());
+System.out.println(pw1.getNumber());
+System.out.println(pw1.getDate());
+
+System.out.println(pw1.getShift());
+System.out.println(pw1.getHPR());
+}} 
